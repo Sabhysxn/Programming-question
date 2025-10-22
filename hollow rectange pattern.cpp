79f675c71c549f94e9da9d624427fc991277e8e2
Sabhy saxena <sabhysaxena9368@gmail.com>
2025-10-22 18:21:45 +0530

    Add files via upload

---
+++ hollow rectange pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int rows,columns;
+    cout<<"Enter the rows : ";
+    cin>>rows;
+    cout<<"Enter the columns : ";
+    cin>>columns;
+    for(int i=1;i<=rows;i++) {
+        for(int j=1;j<=columns;j++) {
+            if(i==1||i==rows){
+                cout<<"* ";
+            }
+            else if (j==1||j==columns) {
+                cout<<"* ";
+            }
+            else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
