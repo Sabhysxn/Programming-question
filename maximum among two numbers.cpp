03b329b50537b9ff86d4d49fba3096913610e6ce
Sabhy saxena <sabhysaxena9368@gmail.com>
2025-10-25 17:58:53 +0530

    Add files via upload

---
+++ maximum among two numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1,num2;
+    cout<<"Enter the value of num1 and num2 : ";
+    cin>>num1>>num2;
+    if(num1>num2) {
+        cout<<"num1 is max number";
+    }
+    else {
+        cout<<"num2 is max number";
+    }
+}
