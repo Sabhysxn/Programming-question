823923d80ef17bd7f8c7d8fa9aa9dc8297f600e7
Sabhy saxena <sabhysaxena9368@gmail.com>
2025-10-29 18:41:06 +0530

    Add files via upload

---
+++ palindrone pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int row;
+    cout<<"Enter the row : ";
+    cin>>row;
+    for(int i=1;i<=row;i++) {
+        int j;
+        for(j=1;j<=row-i;j++) {
+            cout<<"  ";
+        }
+        int k=i;
+        for(;j<=row;j++) {
+            cout<<k--<<" ";
+        }
+        k=2;
+        for(;j<=row+i-1;j++) {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+}
